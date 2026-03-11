6bcd22227c8d9f41abaf88a9ac3480dd450aff7e
Pio <dawidpiotrowski2303@gmail.com>
2026-03-11 10:56:20 +0100

    Initial Commit

---
+++ Atlas/AtlasApp.swift
@@ -0,0 +1,32 @@
+//
+//  AtlasApp.swift
+//  Atlas
+//
+//  Created by Dawid Piotrowski on 11/03/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AtlasApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
